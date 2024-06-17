6b79a68f8afe318d61aa9e2df2163c08ed42d0ad
YuPCypc <838433378@qq.com>
2024-06-17 16:29:04 +0800

    feature: 添加redis常量类

---
+++ admin/src/main/java/com/yupc/myshortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,11 @@
+package com.yupc.myshortlink.admin.common.constant;
+
+/**
+ * 短链接后管redis常量类
+ */
+public class RedisCacheConstant {
+    /**
+     * 用户注册分布式锁
+     */
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
