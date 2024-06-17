8fd6be0ca7aa43123797e6006f559c8f2e8649fa
YuPCypc <838433378@qq.com>
2024-06-17 16:26:38 +0800

    feature: 开发BaseDO

---
+++ admin/src/main/java/com/yupc/myshortlink/admin/common/database/BaseDO.java
@@ -0,0 +1,32 @@
+package com.yupc.myshortlink.admin.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+import java.util.Date;
+
+/**
+ * 数据库持久层对象基础属性
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
+ */
+@Data
+public class BaseDO {
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
