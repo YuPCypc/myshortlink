3064dfab88f8bb37de85a71753e50cd9edd12865
YuPCypc <838433378@qq.com>
2024-06-26 16:32:07 +0800

    feature: 前端传递有效期时间序列化

--- project/src/main/java/com/yupc/myshortlink/project/dto/req/ShortLinkCreateReqDTO.java
+++ project/src/main/java/com/yupc/myshortlink/project/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package com.yupc.myshortlink.project.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -36,6 +37,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date validTime;
 
     /**
