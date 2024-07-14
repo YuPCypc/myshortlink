e4bd3a9fdee3a8ec6327a42195a78edd9500d6c3
YuPCypc <838433378@qq.com>
2024-07-14 20:32:20 +0800

    optimize: 优化单个用户创建分组数量

--- admin/src/main/java/com/yupc/myshortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/yupc/myshortlink/admin/common/constant/RedisCacheConstant.java
@@ -8,4 +8,10 @@ public class RedisCacheConstant {
      * 用户注册分布式锁
      */
     public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
+
 }
--- admin/src/main/java/com/yupc/myshortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/yupc/myshortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,10 +1,12 @@
 package com.yupc.myshortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yupc.myshortlink.admin.common.biz.user.UserContext;
+import com.yupc.myshortlink.admin.common.convention.exception.ClientException;
 import com.yupc.myshortlink.admin.common.convention.result.Result;
 import com.yupc.myshortlink.admin.dao.entity.GroupDO;
 import com.yupc.myshortlink.admin.dao.mapper.GroupMapper;
@@ -16,16 +18,27 @@ import com.yupc.myshortlink.admin.remote.dto.ShortLinkRemoteService;
 import com.yupc.myshortlink.admin.remote.dto.resp.ShortLinkCountQueryRespDTO;
 import com.yupc.myshortlink.admin.service.GroupService;
 import com.yupc.myshortlink.admin.toolkit.RandomNumberGenerator;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import static com.yupc.myshortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
+
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    private final RedissonClient redissonClient;
+    @Value("${short-link.group.max-num}")
+    private Integer groupMaxNum;
 
     /**
      * 后续spring-cloud Feign调用
@@ -42,17 +55,30 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
     }
     @Override
     public void saveGroup(String username,String groupName) {
-        String gid;
-        do {
-            gid = RandomNumberGenerator.generateRandomString();
-        } while (!hasGid(username,gid));
-        GroupDO groupDO = GroupDO.builder()
-                .gid(gid)
-                .sortOrder(0)
-                .username(username)
-                .name(groupName)
-                .build();
-        baseMapper.insert(groupDO);
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(String.format("已超出最大分组数：%d", groupMaxNum));
+            }
+            String gid;
+            do {
+                gid = RandomNumberGenerator.generateRandomString();
+            } while (!hasGid(username, gid));
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .sortOrder(0)
+                    .username(username)
+                    .name(groupName)
+                    .build();
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
